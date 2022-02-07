492d619756326d504006a4e74f19898cd404eebb
mikefrostov <m.e.morozov1@gmail.com>
2022-02-07 05:36:32 -0500

    fix mongodb

--- js/nodejs_blogapp/index.js
+++ js/nodejs_blogapp/index.js
@@ -47,7 +47,7 @@ app.use(expressSession({
 app.use(fileUpload());
 app.use(express.static('public'));
 app.use(expressEdge);
-mongoose.connect('mongodb://localhost:27017/node-blog', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27018/node-blog', { useNewUrlParser: true })
     .then(() => 'You are now connected to Mongo!')
     .catch(err => console.error('Something went wrong', err))
 
