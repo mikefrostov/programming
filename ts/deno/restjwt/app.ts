0c22f78578663099b8c5bfb9189f6cd2f46ab2c7
morozovme <m.e.morozov1@gmail.com>
2022-03-08 14:00:09 +0300

    still working

--- ts/deno/restjwt/app.ts
+++ ts/deno/restjwt/app.ts
@@ -1,11 +1,15 @@
 // https://habr.com/ru/company/otus/blog/532582/
+// https://github.com/Trobyss/deno-api-rest-jwt/tree/master/helpers
 
 
 // app.ts
 import { Application, Router, Status } from "https://deno.land/x/oak/mod.ts";
 
 import { DatabaseController } from "./controllers/Database.ts";
-import { UserRoutes } from "./routers/UserRoute.ts";
+
+import { UserRoutes, PrivateRoutes } from "./routers/index.ts";
+
+import { logger } from "./middlewares/logger.ts";
 
 // Initialise app
 const app = new Application();
@@ -21,8 +25,16 @@ router.get("/", (ctx) => {
 
 const userRoutes = UserRoutes(router);
 
+// Logger
+app.use(logger);
+
+
 app.use(router.routes());
 app.use(router.allowedMethods());
+
+const privateRoutes = PrivateRoutes(router);
+app.use(privateRoutes.routes());
+app.use(privateRoutes.allowedMethods());
 
 app.use(userRoutes.routes());
 app.use(userRoutes.allowedMethods());
