54596a626505ae2ef605125c5612c880af594b43
morozovme <m.e.morozov1@gmail.com>
2022-04-26 04:42:20 -0400

    add some file truncating

--- jobtask/python/main.py
+++ jobtask/python/main.py
@@ -4,15 +4,27 @@
 from datetime import datetime, timedelta
 import logging
 
+
+path = 'logfile.log'
+
 def generateLogs():
-    with open('logfile.log', 'a') as f:
+    with open(path, 'a') as f:
         for i in range(1, 10):
             d = datetime.today() - timedelta(days=i)
             print("writing: " + str(d))
             print(str(d), file=f)
 
 def cutLogs():
-    pass
+    try:
+        with open(path, 'w') as f:
+            lines = f.readlines()
+            for line in lines:
+                if line.strip() in range(datetime.today(), datetime.today() - timedelta(days=7)):
+                    
+
+    except IOError:
+        print('Failure')
+    
 
 if __name__ == "__main__":
     generateLogs()
