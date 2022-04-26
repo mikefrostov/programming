79d6bcec7a340b9ce816950b0485fd37a42c57b4
morozovme <m.e.morozov1@gmail.com>
2022-04-26 02:46:11 -0400

    add log generator

--- jobtask/python/main.py
+++ jobtask/python/main.py
@@ -1,18 +1,22 @@
 #!/bin/python3
 
 
-from datetime import datetime
-
+from datetime import datetime, timedelta
+import logging
 
 def generateLogs():
-    pass
+    with open('logfile.log', 'a') as f:
+        for i in range(1, 10):
+            d = datetime.today() - timedelta(days=i)
+            print("writing: " + str(d))
+            print(str(d), file=f)
 
 def cutLogs():
     pass
 
 if __name__ == "__main__":
     generateLogs()
-    cutLogs()
+#    cutLogs()
 
 
 
