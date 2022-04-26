4e7be5e370c3e5f60d75b20eb0953ee1dfd94218
morozovme <m.e.morozov1@gmail.com>
2022-04-26 01:58:01 -0400

    initial commit

---
+++ jobtask/python/main.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+
+from datetime import datetime
+
+
+def generateLogs():
+    pass
+
+def cutLogs():
+    pass
+
+if __name__ == "__main__":
+    generateLogs()
+    cutLogs()
+
+
+
+
+
