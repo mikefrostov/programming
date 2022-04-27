4692068096cea778fb340425efe3e094cd8b3c88
morozovme <m.e.morozov1@gmail.com>
2022-04-27 03:19:51 -0400

    still suffering through python nuts and bolts of date parsing

--- jobtask/python/main.py
+++ jobtask/python/main.py
@@ -5,13 +5,14 @@ from datetime import datetime, timedelta
 import logging
 
 
+
 path = 'logfile.log'
 
 def generateLogs():
     with open(path, 'a') as f:
         for i in range(1, 10):
             d = datetime.today() - timedelta(days=i)
-            print("writing: " + str(d))
+            #print("writing: " + str(d))
             print(str(d), file=f)
 
 def cutLogs():
@@ -19,10 +20,25 @@ def cutLogs():
     try:
         with open(path, 'r+') as f:
             lines = f.readlines()
-            print(str(lines))
+            base = datetime.today()
+            date_list = [base - timedelta(days=x) for x in range(7)]
+            print(date_list)
             for line in lines:
-                print(str(line))
+                #datetoday = str(datetime.today()).split()[0]
+                #print(line.split()[0])
+                #print(line)
+                #print(date_list[0])
+                #print(date_list)
                 #if line.strip() in range(datetime.today(), datetime.today() - timedelta(days=7)):
+                #if line in date_list:
+                print(line)
+                #print(datetime.strptime(line.split()[0], "%Y-%m-%d"))
+                #print(datetime.strptime(, "%Y-%m-%d"))
+                if datetime.strptime(line.split()[0], "%Y-%m-%d") in date_list:        
+                    #print("it is in date_list: "+ line)
+                    print("line is in date_list: " + line)
+                    pass
+
                 #    print(str(line))
     except IOError as e:
         print('Failure', e)
