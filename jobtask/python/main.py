e64d3649ae397aee1b31de4a05e20371782b6ffe
morozovme <m.e.morozov1@gmail.com>
2022-04-28 15:33:11 -0400

    log formatting kept only date

--- jobtask/python/main.py
+++ jobtask/python/main.py
@@ -11,8 +11,7 @@ path = 'logfile.log'
 def generateLogs():
     with open(path, 'a') as f:
         for i in range(1, 10):
-            d = datetime.today() - timedelta(days=i)
-            #print("writing: " + str(d))
+            d = datetime.today().date() - timedelta(days=i)
             print(str(d), file=f)
 
 def cutLogs():
@@ -20,26 +19,12 @@ def cutLogs():
     try:
         with open(path, 'r+') as f:
             lines = f.readlines()
-            base = datetime.today()
+            base = datetime.today().date()
             date_list = [base - timedelta(days=x) for x in range(7)]
             print(date_list)
             for line in lines:
-                #datetoday = str(datetime.today()).split()[0]
-                #print(line.split()[0])
-                #print(line)
-                #print(date_list[0])
-                #print(date_list)
-                #if line.strip() in range(datetime.today(), datetime.today() - timedelta(days=7)):
-                #if line in date_list:
-                print(line)
-                #print(datetime.strptime(line.split()[0], "%Y-%m-%d"))
-                #print(datetime.strptime(, "%Y-%m-%d"))
-                if datetime.strptime(line.split()[0], "%Y-%m-%d") in date_list:        
-                    #print("it is in date_list: "+ line)
-                    print("line is in date_list: " + line)
-                    pass
-
-                #    print(str(line))
+                if datetime.strptime(line.strip(), "%Y-%m-%d").date() in date_list:
+                    print("it is in date_list: "+ line)
     except IOError as e:
         print('Failure', e)
     
