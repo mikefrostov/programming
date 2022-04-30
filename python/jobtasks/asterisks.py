63b8825bb301df0e7d0a25387991b42b7f2daa71
morozovme <m.e.morozov1@gmail.com>
2022-04-30 08:56:04 -0400

    add asterisks

---
+++ python/jobtasks/asterisks.py
@@ -0,0 +1,19 @@
+# task:
+# given a rectangular matrix of characters, add a border of asterisks ( * ) to it
+# example:
+# for 
+# list    = ["abc",
+#            "ded"]
+# 
+# output  = ["*****",
+#            "*abc*",
+#            "*ded*",
+#            "*****"]
+# 
+# 1 <= picture.length <= 100
+# 1 <= picture[i].length <= 100
+# 
+#
+
+
+def asterisks()
