d2cabb6cb355d313e7308fbafa1615fd9693eb2f
morozovme <m.e.morozov1@gmail.com>
2022-05-06 08:08:23 -0400

    it works, yet again

--- js/nodejs_streaming_app/backend/app.js
+++ js/nodejs_streaming_app/backend/app.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
 const app = express();
+
+
 app.use(cors())
 
-app.get('/video', (req, res) => {
-    res.sendFile('assets/video1.mp4', { root: __dirname });
-});
+const Videos = require('./routes/Video')
+app.use('/videos', Videos)
 
 app.listen(5000, () => {
     console.log('Listening on port 5000!')
