41e39cb9faf55a0376f3f8e709d02288161eed55
mikefrostov <m.e.morozov1@gmail.com>
2022-07-02 23:09:55 +0300

    add exitcode

--- python/jobtasks/healthcheckparse.py
+++ python/jobtasks/healthcheckparse.py
@@ -1,27 +1,8 @@
 import sys
 import requests
-import time
-import urllib
-import re
-#import validators
-from pyquery import PyQuery    
 
 
 if __name__ == "__main__":
-    #counter = int(sys.argv[1])
-    #print(sys.argv[1])
-    
-    #print (url)
-#    if validators.url(url):
-#        while(True):
-#            r = requests.get(url)    
-#            if r.status_code == 200:
-#                html = r
-#            if r.status_code != 200:
-#                print("ERR({" + str(r.status_code) + "})")
-#            time.sleep(counter)
-#    else:
-#        print("URL parsing error: " + url)
     url = sys.argv[1]
     r = requests.get(url)
     result = str(r.content).split("\\n")
@@ -34,10 +15,4 @@ if __name__ == "__main__":
     for k,v in dict1.items():
         if v != ' OK':
             print(" problem : " + k + " is " + v)
-
-    #print(re.split('; |, |\*|\\n\'\n', str(r.content)))
-    #d = dict(s.split(':') for s in result)
-    #print(d)
-    #pq = PyQuery(html)
-    #tag = pq('pre')
-    #print tag.text()
+            sys.exit(-1)
