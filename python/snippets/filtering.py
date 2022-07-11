17eef612de7ec8af8f528cf3ce0740611f6a98d8
mikefrostov <m.e.morozov1@gmail.com>
2022-07-11 12:35:05 +0300

    filter

---
+++ python/snippets/filtering.py
@@ -0,0 +1,22 @@
+mylist = [1, 4, -5, 10, -7, 2, 3, -1]
+
+[n for n in mylist if n > 0]
+
+
+pos = (n for n in mylist if n > 0)
+for x in pos:
+    print(x)
+
+
+values = ['1', '2', '-3', '-', '4', 'N/A', '5']
+
+def is_int(val):
+    try:
+        x = int(val)
+        return True
+    except ValueError:
+        return False
+
+ivals = list(filter(is_int, values))
+print(ivals)
+# Outputs ['1', '2', '-3', '4', '5']
