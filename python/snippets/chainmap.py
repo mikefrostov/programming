5072832f0661a9f5aa025da69576455d0bcb9b5e
mikefrostov <m.e.morozov1@gmail.com>
2022-07-11 12:34:34 +0300

    chainmap

---
+++ python/snippets/chainmap.py
@@ -0,0 +1,20 @@
+a = {'x': 1, 'z': 3 }
+b = {'y': 2, 'z': 4 }
+
+
+from collections import ChainMap
+c = ChainMap(a,b)
+print(c['x']) # Outputs 1 (from a)
+print(c['y']) # Outputs 2 (from b)
+print(c['z']) # Outputs 3 (from a)
+
+values = ChainMap()
+values['x'] = 1
+
+# Add a new mapping
+values = values.new_child()
+values['x'] = 2
+
+# Add a new mapping
+values = values.new_child()
+values['x'] = 3
